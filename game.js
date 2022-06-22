0469d0efb21b119020c9cbd83e519b35dd77b11c
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-22 09:36:19 +0800

    [update] step 3 add flash and sound

--- game.js
+++ game.js
@@ -6,9 +6,12 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[nextSequence()];
+    gamePattern.push(randomChosenColour);
 
-    return randomNumber;
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+    
 }
-
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
