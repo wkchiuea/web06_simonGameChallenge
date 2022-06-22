ad1229cea1d4b36c85ac5e871ae5d8a7eb675fee
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-22 10:28:08 +0800

    [update] step 5 refactor play sound

--- game.js
+++ game.js
@@ -9,6 +9,8 @@ $(".btn").on("click", function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
+    playSound(userChosenColour);
+
 });
 
 function nextSequence() {
@@ -19,8 +21,14 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
     
+}
+
+function playSound(name) {
+
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+
 }
 
