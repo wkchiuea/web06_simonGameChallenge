47b20770fc2aafe3ec08ffeaed39701e321a6551
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-22 13:02:35 +0800

    [update] step 8 check answer

--- game.js
+++ game.js
@@ -23,6 +23,7 @@ $(".btn").on("click", function() {
     playSound(userChosenColour);
     animatePress(userChosenColour);
 
+    checkAnswer(userClickedPattern.length-1)
 });
 
 function nextSequence() {
@@ -55,3 +56,27 @@ function animatePress(currentColour) {
     }, 100);
 
 }
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+  
+        if (userClickedPattern.length === gamePattern.length){
+  
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+  
+        }
+  
+      } else {
+  
+        console.log("wrong");
+  
+      }
+
+}
+
+
