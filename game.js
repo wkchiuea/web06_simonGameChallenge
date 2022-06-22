8b281b7e8185faeebcc264dfcfe050f1c98b1792
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-22 09:21:15 +0800

    [update] step 2 create pattern

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
 
 
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 3);
+
+    return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+gamePattern.push(randomChosenColour);
