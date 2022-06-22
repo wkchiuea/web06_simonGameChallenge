ad9c0c5ed140d40ce74564c282ff2c562c445c0f
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-22 09:24:05 +0800

    [fix] fix randomNumber

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
 
-    var randomNumber = Math.floor(Math.random() * 3);
+    var randomNumber = Math.floor(Math.random() * 4);
 
     return randomNumber;
 }
